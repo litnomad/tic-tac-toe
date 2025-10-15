d81d4288a67667f53200ba7427a40ab7b9b8bc06
Lenna <128262375+waxingrose@users.noreply.github.com>
2025-10-15 16:15:32 -0400

    associate game console with HTML DOM elements. user will be able to play the game by clicking the buttons on the board. user will be able to enter their player name.

--- script.js
+++ script.js
@@ -13,8 +13,17 @@ function gameBoard() {
     }
 
     function updateBoard(row, column) {
-        board[row].splice(column, 1, activePlayer.token);
-        console.log(board);
+
+        // will only add token to empty square
+        if (board[row][column] === '') {
+            board[row].splice(column, 1, activePlayer.token);
+            console.log(board);
+            console.log(`${activePlayer.player}` + '`s turn');
+        }
+        else {
+            null;
+        }
+
         win();
     }
 
@@ -23,54 +32,102 @@ function gameBoard() {
         // for first player
         if (board[0][0] == players[0].token && board[0][1] == players[0].token && board[0][2] == players[0].token) {
             console.log(`${players[0].player}` + ' wins!');
+
+            winText.innerHTML = `${players[0].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[1][0] == players[0].token && board[1][1] == players[0].token && board[1][2] == players[0].token) {
             console.log(`${players[0].player}` + ' wins!');
+
+            winText.innerHTML = `${players[0].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[2][0] == players[0].token && board[2][1] == players[0].token && board[2][2] == players[0].token) {
             console.log(`${players[0].player}` + ' wins!');
+
+            winText.innerHTML = `${players[0].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[0][0] == players[0].token && board[1][0] == players[0].token && board[2][0] == players[0].token) {
             console.log(`${players[0].player}` + ' wins!');
+
+            winText.innerHTML = `${players[0].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[0][1] == players[0].token && board[1][1] == players[0].token && board[2][1] == players[0].token) {
             console.log(`${players[0].player}` + ' wins!');
+
+            winText.innerHTML = `${players[0].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[0][2] == players[0].token && board[1][2] == players[0].token && board[2][2] == players[0].token) {
             console.log(`${players[0].player}` + ' wins!');
+
+            winText.innerHTML = `${players[0].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[0][0] == players[0].token && board[1][1] == players[0].token && board[2][2] == players[0].token) {
             console.log(`${players[0].player}` + ' wins!');
+
+            winText.innerHTML = `${players[0].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[0][2] == players[0].token && board[1][1] == players[0].token && board[2][0] == players[0].token) {
             console.log(`${players[0].player}` + ' wins!');
+
+            winText.innerHTML = `${players[0].player}` + ' wins!';
+            header.appendChild(winText);
         }
         // for second player
         else if (board[0][0] == players[1].token && board[0][1] == players[1].token && board[0][2] == players[1].token) {
             console.log(`${players[1].player}` + ' wins!');
+
+            winText.innerHTML = `${players[1].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[1][0] == players[1].token && board[1][1] == players[1].token && board[1][2] == players[1].token) {
             console.log(`${players[1].player}` + ' wins!');
+
+            winText.innerHTML = `${players[1].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[2][0] == players[1].token && board[2][1] == players[1].token && board[2][2] == players[1].token) {
             console.log(`${players[1].player}` + ' wins!');
+
+            winText.innerHTML = `${players[1].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[0][0] == players[1].token && board[1][0] == players[1].token && board[2][0] == players[1].token) {
             console.log(`${players[1].player}` + ' wins!');
+
+            winText.innerHTML = `${players[1].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[0][1] == players[1].token && board[1][1] == players[1].token && board[2][1] == players[1].token) {
             console.log(`${players[1].player}` + ' wins!');
+
+            winText.innerHTML = `${players[1].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[0][2] == players[1].token && board[1][2] == players[1].token && board[2][2] == players[1].token) {
             console.log(`${players[1].player}` + ' wins!');
+
+            winText.innerHTML = `${players[1].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[0][0] == players[1].token && board[1][1] == players[1].token && board[2][2] == players[1].token) {
             console.log(`${players[1].player}` + ' wins!');
+
+            winText.innerHTML = `${players[1].player}` + ' wins!';
+            header.appendChild(winText);
         }
         else if (board[0][2] == players[1].token && board[1][1] == players[1].token && board[2][0] == players[1].token) {
             console.log(`${players[1].player}` + ' wins!');
+
+            winText.innerHTML = `${players[1].player}` + ' wins!';
+            header.appendChild(winText);
         }
-        else { null; }
+        else { false; }
 
     }
 
@@ -83,20 +140,21 @@ function gameBoard() {
 
 let activePlayer;
 
-function gameController() {
+function gameController(firstPlayer) {
 
     const board = gameBoard();
 
-    const players = [
-        {
-            player: 'firstPlayer',
-            token: "x"
-        },
-        {
-            player: 'secondPlayer',
-            token: "o"
-        }
-    ]
+    const players = 
+        [
+            {
+                player: firstPlayer,
+                token: "x"
+            },
+            {
+                player: 'Second player',
+                token: "o"
+            }
+        ]
 
     function switchPlayer() {
 
@@ -113,10 +171,9 @@ function gameController() {
             false;
         }
 
-        return activePlayer, console.log(`${activePlayer.player}` + '`s turn');
+        return activePlayer;
     }
 
-    /* revise code */
     const addToken = (row, column) => {
 
         switchPlayer();
@@ -132,8 +189,33 @@ function gameController() {
 
 }
 
-const play = gameController().addToken;
-play(1, 1);
-play(0, 0);
-play(1, 0);
-play(0, 1);
+const play = gameController();
+
+
+// attach game to DOM element 
+const header = document.querySelector('header');
+const buttons = document.querySelectorAll('#cell');
+const winText = document.createElement('h3');
+const submit = document.querySelector('.submit');
+
+winText.style.color = 'red';
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        play.addToken(button.dataset.row, button.dataset.column);
+
+        if (!button.innerText) {
+            button.innerText = activePlayer.token;
+        }
+    }
+    )
+}
+)
+
+// user enters player name 
+submit.addEventListener('click', (e) => {
+    const firstPlayer = document.querySelector('#name').value;
+    play.players[0].player = firstPlayer;
+
+    e.preventDefault();
+})
